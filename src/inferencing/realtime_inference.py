bfc129ff47ada0df50ebf4eb7e57032e6838fbef
HemachandranD <HemachandranD>
2025-01-26 13:55:05 +0000

    updated the inference file

---
+++ src/inferencing/realtime_inference.py
@@ -0,0 +1,13 @@
+from databricks.sdk import WorkspaceClient
+from src.config.configuration import serving_endpoint_name
+
+def main(question = "What is PPO?"):
+    ws_client = WorkspaceClient()
+
+    answer = ws_client.serving_endpoints.query(serving_endpoint_name, inputs=[{"input": question}])
+    return answer
+
+
+if __name__ == "__main__":
+    main()
+    
